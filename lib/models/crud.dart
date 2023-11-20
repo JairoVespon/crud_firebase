a300237de93febeabb2621749a882b0f9f037dd1
JairoVespon <108412797+JairoVespon@users.noreply.github.com>
2023-11-19 21:47:15 -0600

    consulta de datos

---
+++ lib/models/crud.dart
@@ -0,0 +1,20 @@
+mport 'package:cloud_firestore/cloud_firestore.dart';
+
+FirebaseFirestore db = FirebaseFirestore.instance;
+
+/**Metodo para consultar toda la informacion de la coleccion tb_students */
+Future<List> getStudiantes() async {
+  List students = [];
+  CollectionReference collectionReference = db.collection('tb_students');
+  QuerySnapshot querySnapshot = await collectionReference.get();
+  /*
+  for (var documento in querySnapshot.docs) {
+  students.add(documento.data());
+  }
+  */
+  querySnapshot.docs.forEach((documento) {
+  students.add(documento.data());
+  });
+  //print('$students');
+  return students;
+}
